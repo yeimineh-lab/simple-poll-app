312e89340996bbbf61b3b4323ec7dc124ddbcd0b
yeimineh-lab <yeimineh@outlook.com>
2026-02-05 14:21:18 +0100

    chore: serve only client folder as static

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../..")));     
+app.use(express.json());   
 app.use(express.static(path.join(__dirname, "../../client"))); 
 
 
